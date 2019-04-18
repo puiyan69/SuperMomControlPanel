17b06a97e2277153d7d46292c855a10d5e6db005
puiyan69 <puiyan69@gmail.com>
2019-04-18 22:53:40 +0800

    Login UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,93 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html>
 
-        <title>Laravel</title>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+<style>
+body {
+    background-color : #273953;
+}
+.email-div, .password-div{
+    margin-right : 30px;
+}
+.email-div input, .password-div input{
+    outline: 0;
+    color : white;
+    height : 45px;
+    border-width: 0 0 2px;
+    border-color : grey;
+    background-color : #273A53; 
+}
+#email, #password {
+    border-radius : 10px;
+}
+#email:focus, #password:focus {
+    border-color : #8EFAA3;
+}
+#logInButton{
+    width : 425px;
+    font-weight : bold;
+    height : 45px;
+    background-color : #53A5A7;
+    margin-right : 30px;
+    border:none;
+}
+#logo{
+    height : 200px;
+    width : 200px;
+    margin-top : 30px;
+}
+.verticalLine {
+  border-left: 1px solid grey;
+}
+.form-group{
+    display : flex;
+    margin-top : 60px;
+    justify-content:center;
+}
+#title{
+    color : white;
+    text-align : center;
+    font-family : Comic Sans MS;
+}
+#footer{
+    color : grey;
+}
+.submit-div{
+    margin-top : 50px;
+}
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+</style>
+<body>
+    <div align="center">
+    <figure>
+        <img src="image\AppLogo.png" class="rounded float-left" id="logo" alt="...">
+    </figure>
+    <h3 id="title">Super Mom Control Panel</h3>
+    </div>
+    <div class="verticalLine"></div>
+    <form>
+        <div class="form-group" align=center>
+            <div class="input-group mb-3 email-div">
+                <input type="text" class="form-control" id="email" placeholder="Email" aria-label="Username" aria-describedby="basic-addon1">
+            </span>
             </div>
+             <br>
+            <div class="input-group mb-3 password-div">
+                <input type="password" class="form-control" id="password" placeholder="Password" aria-label="Username" aria-describedby="basic-addon1">
+            </div>
+            <br>
         </div>
-    </body>
+        <div class="submit-div" align=center>
+            <button type="submit" class="btn btn-primary" id="logInButton">SIGN IN</button>
+        </div>
+    </form>
+    </div>
+    <footer class="footer navbar-fixed-bottom"id="footer">&copy; Copyright 2019 Chow Pui Yan</footer>
+</body>
 </html>
